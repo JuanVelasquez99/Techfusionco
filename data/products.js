d1e1bfc82f32fc31b6c4ad2ac9ca660eabaa6ff9
JuanVelasquez99 <guerreroesteban.99@gmail.com>
2026-01-21 10:39:06 -0500

    Update image paths for products in products.js

--- data/products.js
+++ data/products.js
@@ -3,14 +3,14 @@ const products = [
     id: 1,
     name: "Laptop Gamer ASUS",
     price: 3500000,
-    image: "assets/img/laptop.jpg",
+    image: "assets/img/images/laptop.jpg",
     description: "Ryzen 7, 16GB RAM, RTX 3060"
   },
   {
     id: 2,
     name: "iPhone 14 Pro",
     price: 4800000,
-    image: "assets/img/iphone.jpg",
+    image: "assets/img/images/iphone.jpg",
     description: "256GB, Cámara Pro"
   }
 ];
